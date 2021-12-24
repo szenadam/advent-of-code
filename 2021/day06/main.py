7a83b6e91d957e93f8660591d324d19e52c0dc8c
szenadam <szenadam1@gmail.com>
2021-12-24 14:08:10 +0100

    Add 2021 day06 part 2 solution

--- 2021/day06/main.py
+++ 2021/day06/main.py
@@ -1,27 +1,24 @@
 
 def main():
     data = []
+
     with open('input.txt', 'r') as f:
         line = f.readline()
         for i in line.split(','):
             data.append(int(i))
-    print(data)
 
-    for i in range(1, 80 + 1):
-        zero_count = 0
-        for j, k in enumerate(data):
-            data[j] = data[j] - 1
-            if k == 0:
-                zero_count += 1
-                data[j] = 6
-        if zero_count > 0:
-            for i in range(zero_count):
-                data.append(8)
-        # print(data)
+    # get the count of the fish with the same number of days left
+    fish = [data.count(i) for i in range(9)]
 
-    print(len(data))
+    for i in range(256):
+        num = fish.pop(0)
+        fish[6] += num
+        fish.append(num)
+
+    print(sum(fish))
 
     return 0
+
 
 if __name__ == '__main__':
     main()
