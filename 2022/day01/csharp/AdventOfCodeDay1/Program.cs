2238077f35f3af5f5c0da53d8a017db13d1b6a56
szenadam <szenadam1@gmail.com>
2023-01-03 14:42:15 +0100

    Add 2022-01 C# solution

---
+++ 2022/day01/csharp/AdventOfCodeDay1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace AdventOfCodeDay1;
+
+public static class Program
+{
+    private static void Main(string[] args)
+    {
+        IOrderedEnumerable<int> elfs = File.ReadAllText("input.txt")
+            .Split("\n\n")
+            .Select(elf => elf.Split("\n", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Sum())
+            .OrderByDescending(x => x);
+        Console.WriteLine($"Most carry: {elfs.Max()}");
+        Console.WriteLine($"Sum of top three carriers: {elfs.Take(3).Sum()}");
+    }
+}
